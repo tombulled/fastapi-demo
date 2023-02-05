f9b740ba2fa97e2c64110ab7c45e64a8fbf2f95b
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2023-02-05 21:59:05 +0000

    API: Add some example middleware

--- api/api/main.py
+++ api/api/main.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Request, Response
 
 from .dependencies import auth
 from .router import router
@@ -6,5 +6,13 @@ from .router import router
 app: FastAPI = FastAPI(
     # dependencies=[Depends(auth)],
 )
+
+
+@app.middleware("http")
+def log_request(request: Request, call_next) -> Response:
+    print("Logging Request:", request.__dict__)
+
+    return call_next(request)
+
 
 app.include_router(router, prefix="/users")
